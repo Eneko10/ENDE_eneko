d70d5d7b39306ed7a8d1a86c54d89567530513ea
ikaslea <ikaslea@DESKTOP-DSP6VMJ.mshome.net>
2020-11-17 09:08:55 +0100

    "Menu 4 opciones"

--- menuakGit/src/menuak.java
+++ menuakGit/src/menuak.java
@@ -3,7 +3,10 @@ public class menuak {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("1.Primera opcion");
+		System.out.println("2.Segunda opcion");
+		System.out.println("3.Tercera opcion");
+		System.out.println("4.Cuarta opcion");
 	}
 
 }
